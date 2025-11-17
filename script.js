f085fbbeb1e6f33c04e2b0f3a55d083ec1292484
Ibun Abbas <yusufibunabbas@gmail.com>
2025-11-17 17:02:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,89 @@
-document.getElementById('calculateBtn').addEventListener('click',calculatetax)
+const minutesLabel = document.getElementById('minutes');
+const secondsLabel = document.getElementById('seconds');
+const millisecondsLabel = document.getElementById('milliseconds');
 
-function calculatetax(){
-    const salary = parseFloat(document.getElementById('salary').value);
-    const allowance = parseFloat(document.getElementById('allowance').value);
-    
-    if(isNaN(salary) || isNaN(allowance)){
-        alert("Please fill in the space below")
-    }
+const startButton = document.getElementById('startBtn');
+const stopButton = document.getElementById('stopBtn');
+const pauseButton = document.getElementById('pauseBtn');
+const resetButton = document.getElementById('resetBtn');
 
-    const Monthlypay = salary + allowance;
-    const housing = 3/100 * Monthlypay;
-    const hospitaling = 2/100 * Monthlypay;
-    const leave = 1.5/100 * Monthlypay;
-    const tax = 1.5/100 * Monthlypay;
-    const insurance = 2/100 * Monthlypay;
-    const vat = 1.5/100 * Monthlypay;
-    const Totaltax = housing + hospitaling + leave + tax + insurance + vat;
-    const grosspay = Monthlypay - Totaltax;
-    const annualpay = grosspay**2;
+const lapList = document.getElementById('laplist');
 
-    displayResult(Monthlypay, housing, hospitaling, leave, tax, insurance, vat, Totaltax, grosspay, annualpay)
+/// stopwatch variables
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let interval;
+
+startButton.addEventListener('click',startTimer);
+stopButton.addEventListener('click',stopTimer);
+pauseButton.addEventListener('click',pauseTimer);
+resetButton.addEventListener('click',resetTimer);
+
+
+function startTimer(){
+
+    interval =  setInterval(updateTimer,10);
+    startButton.disabled = true;
+
 }
 
-function displayResult(Monthlypay, housing, hospitaling, leave, tax, insurance, vat, Totaltax, grosspay, annualpay){
+function stopTimer(){
 
-    const resultDiv = document.getElementById("Taxresults");
+    clearInterval(interval);
+    addToLapList();
+    resetTimerData();
+    startButton.disabled = false;
+}
 
-    resultDiv.innerHTML = `
-        <h4>Result<h4>
-        <table>
-        <tr class="tablehead">
-            <th>Variable</th>
-            <td>Percentage</td>
-            <td>Amount</td>
-        </tr>
-        <tr>
-            <th>Housing</th>
-            <td>3%</td>
-            <td>N${housing.toFixed(2)}</td>
-        </tr>
-        <tr>
-            <th>Hospitaling</th>
-            <td>2%</td>
-            <td>N${hospitaling.toFixed(2)}</td>
-        </tr>
-        <tr>
-            <th>Leave</th>
-            <td>1.5%</td>
-            <td>N${leave.toFixed(2)}</td>
-        </tr>
-        <tr>
-            <th>Tax</th>
-            <td>1.5%</td>
-            <td>N${tax.toFixed(2)}</td>
-        </tr>
-        <tr>
-            <th>Insurance</th>
-            <td>2%</td>
-            <td>N${insurance.toFixed(2)}</td>
-        </tr>
-        <tr>
-            <th>vat</th>
-            <td>1.5%</td>
-            <td>N${vat.toFixed(2)}</td>
-        </tr>
-        <tr>
-            <th>TOTAL</th>
-            <td></td>
-            <td>N${Totaltax.toFixed(2)}</td>
-        </tr>
-        </table>
-        <p>Grosspay: N${grosspay.toFixed(2)}</p>
-        <p>Annualpay: N${annualpay.toFixed(2)}</p>
-    `;
+function pauseTimer(){
+    clearInterval(interval);
+    startButton.disabled = false;
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    resetTimerData();
+    startButton.disabled = false;
+
+}
+
+function updateTimer(){
+    milliseconds++;
+    if(milliseconds === 100){  //// 1000  -> 1 seconds = 1000 millseconds
+        milliseconds = 0;
+        seconds++;
+        if(seconds === 60){
+            seconds = 0;
+            minutes++;
+        }
+    }
+
+    displayTimer();
+}
+
+function displayTimer(){
+    millisecondsLabel.textContent = padTime(milliseconds);
+    secondsLabel.textContent = padTime(seconds);
+    minutesLabel.textContent = padTime(minutes);    
+}
+
+function padTime(time){
+    return time.toString().padStart(2,'0');
+}
+
+function resetTimerData(){
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    displayTimer();
+}
+
+function addToLapList(){
+    const lapTime = `${padTime(minutes)}:${padTime(seconds)}:${padTime(milliseconds)}`;
+
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `<span>Lap ${lapList.childElementCount + 1}: </span>${lapTime}`;
+    lapList.appendChild(listItem);
 }
